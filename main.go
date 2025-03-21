f353a072106fecd13b9e2c25ba72c36ca351c256
jtsang4 <info@jtsang.me>
2025-03-22 05:40:06 +0800

    chore: add GoReleaser configuration and versioning to the project

--- main.go
+++ main.go
@@ -7,8 +7,19 @@ import (
 	"kick/cmd"
 )
 
+// 通过 ldflags 注入的版本信息
+var (
+	version = "dev"
+	commit  = "none"
+	date    = "unknown"
+)
+
 func main() {
 	rootCmd := cmd.NewRootCmd()
+
+	// 添加版本信息
+	rootCmd.Version = fmt.Sprintf("%s (commit: %s, built at: %s)", version, commit, date)
+
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
